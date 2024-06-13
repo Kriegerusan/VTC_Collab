0d0a04bb0c442fe90ebdae7c2498bbbb4b9dddc4
Kriegerusan <kriegergamedev@gmail.com>
2024-06-13 22:32:17 +0200

    amelioration connection

--- model/Connection.php
+++ model/Connection.php
@@ -4,11 +4,12 @@ class Connection
 
     public function getConnect()
     {
-        $propertiesArray = parse_ini_file('../config/connection.ini');
-        $host = $propertiesArray["serveur"];
-        $db = $propertiesArray["db"];
-        $user = $propertiesArray["ut"];
-        $pwd = $propertiesArray["mdp"];
+        $propertiesArray = parse_ini_file('../config/connection.ini',true);
+        $connectionMethod = $propertiesArray['connection localhost'];
+        $host = $connectionMethod["serveur"];
+        $db = $connectionMethod["db"];
+        $user = $connectionMethod["ut"];
+        $pwd = $connectionMethod["mdp"];
         try {
             $db = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
         } catch (PDOException $e) {
