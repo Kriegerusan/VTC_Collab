572df1941024e908a68ae34b1dbf338db578d8d6
abkff <adrien.bikodoroff@gmail.com>
2024-06-13 21:47:20 +0200

    ajout config connection

--- model/Connection.php
+++ model/Connection.php
@@ -4,8 +4,14 @@ class Connection
 
     public function getConnect()
     {
+        $propertiesArray = parse_ini_file('./conf/DB.ini');
+        $host = $propertiesArray["serveur"];
+        $port = $propertiesArray["port"];
+        $db = $propertiesArray["db"];
+        $user = $propertiesArray["ut"];
+        $pwd = $propertiesArray["mdp"];
         try {
-            $db = new PDO("mysql:host=localhost;dbname=vtc", "root", "");
+            $db = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pwd);
         } catch (PDOException $e) {
             echo $e->getMessage();
             die;
